9412bb7a6ce8a765e7b53ea81c89d50de004d3b5
harryfrzz <harikrishnac005@gmail.com>
2026-07-31 20:45:43 +0530

    Initial Commit

---
+++ cove/coveApp.swift
@@ -0,0 +1,17 @@
+//
+//  coveApp.swift
+//  cove
+//
+//  Created by Harikrishna C on 31/07/26.
+//
+
+import SwiftUI
+
+@main
+struct coveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
